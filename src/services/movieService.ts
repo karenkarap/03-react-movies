4a8e38f52e3a8ef1aa07e409031c1a4cea12d973
kar <myspecspace@gmail.com>
2025-08-13 22:12:44 +0400

    Done

---
+++ src/services/movieService.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import type { Movie } from '../types/movie';
+interface MovieHttpResponse {
+  results: Movie[];
+}
+
+const myKey = import.meta.env.VITE_TMDB_TOKEN;
+axios.defaults.headers.common['Authorization'] = `Bearer ${myKey}`;
+
+export const fetchMovies = async (topic: string) => {
+  const response = await axios.get<MovieHttpResponse>(
+    `https://api.themoviedb.org/3/search/movie?query=${topic}&include_adult=false&language=en-US&page=1`
+  );
+
+  return response.data.results;
+};
