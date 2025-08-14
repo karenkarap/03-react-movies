eaa500fa1f14c2e6d861f782e02e6494c290a02b
kar <myspecspace@gmail.com>
2025-08-14 14:37:22 +0400

    fix movieServices

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import type { Movie } from '../types/movie';
+
 interface MovieHttpResponse {
   results: Movie[];
 }
@@ -7,7 +8,7 @@ interface MovieHttpResponse {
 const myKey = import.meta.env.VITE_TMDB_TOKEN;
 axios.defaults.headers.common['Authorization'] = `Bearer ${myKey}`;
 
-export const fetchMovies = async (topic: string) => {
+export const fetchMovies = async (topic: string): Promise<Movie[]> => {
   const response = await axios.get<MovieHttpResponse>(
     `https://api.themoviedb.org/3/search/movie?query=${topic}&include_adult=false&language=en-US&page=1`
   );
